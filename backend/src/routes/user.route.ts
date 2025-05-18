b0ae3f9c929e36721015f83b94bbfe15aeaac8b5
JayantRautela <jayantrautela0205@gmail.com>
2025-05-18 21:43:46 +0530

    feat(user): add user routes

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import { forgotPassword, generateAccessToken, getUser, login, logout, register, resetPassword, sendOtp, verifyOtp } from "../controllers/user.controller";
+import { isAuthenticated } from "../middlewares/isAuthenticated.middleware";
 import { singleUpload } from "../middlewares/multer.middleware";
-import { register } from "../controllers/user.controller";
 
 const router = Router();
 
 router.post('/register', singleUpload, register);
+router.post('/login', login);
+router.get('/logout', isAuthenticated, logout);
+router.post('/sendOtp', sendOtp);
+router.post('/verifyOtp', verifyOtp);
+router.post('/forgotPassword', forgotPassword);
+router.post('/resetPassword', resetPassword);
+router.get('/user', isAuthenticated, getUser);
+router.post('/getAccessToken', generateAccessToken);
 
 export default router
